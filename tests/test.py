06e3b406f325092d67cac91a0488feb444462602
Shibhani <140485226+Shibhani-V@users.noreply.github.com>
2023-12-14 18:43:33 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,6 @@ import json
 from appium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from percy import percy_screenshot
 
